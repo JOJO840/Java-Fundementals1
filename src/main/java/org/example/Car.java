ee774a5e97014792cafb9ef6517af9795a9d4458
JOJO840 <josip840@gmail.com>
2025-03-03 14:19:08 +0100

    Doing some Java fundementals Car class

---
+++ src/main/java/org/example/Car.java
@@ -0,0 +1,21 @@
+package org.example;
+
+public class Car {
+    int score = 100;
+    public static String carBrand(String brand){
+       return brand;
+    }
+    public static String regNumber(String reg){
+        return reg;
+    }
+    public static int maxSpeed(int speed){
+        return speed;
+    }
+    public static String ownerName(String name){
+        return name;
+    }
+    public static String carColor(String color){
+        return color;
+    }
+
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,16 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
-    }
+        String name = "Josip";
+        String lastName = "Jovanovic";
+        System.out.println(name + " " + lastName);
+
+        int x = 25;
+        int y = 23;
+        int result = x + y;
+        System.out.println(result);
+
+    } //Main Paragraph
+
 }
+
